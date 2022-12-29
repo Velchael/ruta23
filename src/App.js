457713a0932bc2f09351261864495a590e297bb7
stalin velchael mendez <115166348+Velchael@users.noreply.github.com>
2022-12-28 21:24:04 -0300

    agregar basename

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 import Login from './pages/Login';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Users from './pages/components/Users';
 import Orders from './pages/components/Orders';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename='/react-dummy'>
        <nav>
          <Link to="/Login">Login </Link>
          <Link to="/Dashboard">Dashboard</Link>
        </nav>
       <Routes>
-        <Route path="/login/" element={<Login />} />
+        <Route path="/react-dummy" element={<Login />} />
         <Route path="/dashboard/" element={<Dashboard />} >
           <Route path="users" element={<Users />} />
           <Route path="orders" element={<Orders />} />
         </Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
